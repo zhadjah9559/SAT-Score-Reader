263a839e4d37bf7032ea857dc44ba79f6cc6dd6a
zhadjah9559 <zhadjah@gmail.com>
2018-02-13 20:18:19 -0500

    Initial version

--- satScores_proj1/main.cpp
+++ satScores_proj1/main.cpp
@@ -1,3 +1,12 @@
+/*==============================================================================
+ Zachary Hadjah                    February 9th 2018
+ Junior Year                       Data Structures/ Algorithm Analysis
+  
+ This program reads the scores of 10 students SAT scores. It reads the math
+ and verbal sections of the SAT test, putting the math scores in the right column,
+ and the verbal scores in the left column. It will also compute the the average 
+ * of all the math averages and verbal averages.
+ =============================================================================*/
 #include <cstdlib>
 #include <iostream>
 #include <iomanip> 
@@ -8,6 +17,10 @@
 
 using namespace std;
 
+/**
+ * 
+ * @return 0
+ */
 int main() 
 {
     int sat[10][2], mathAvg, verbAvg;
@@ -20,23 +33,32 @@ int main()
     void show_results(int sat[10][2], int mathAvg, int verbAvg, double mathStd,  double verbstd);
     bool again();
 
-    
     describe_program();
     read_scores(sat);
     compute_means(sat, mathAvg, verbAvg);
     compute_std(sat, mathAvg, verbAvg,  mathStd, verbStd);
     //show_results(sat, mathAvg, verbAvg, mathStd, verbStd);
     //again();
-
-    
     return 0;
 }
 
+/*
+ * 
+ */
 void describe_program()
 {
-    
+    cout<<"This program reads SAT scores from 10 students (Math and verbal scores)"
+        <<" and allocates the scores as elements inside a two dimensional array."
+        <<endl<<"The program will then calculate the mean Math SAT score, the mean "
+        <<"Verbal SAT score, and the standard deviation for both subjects."<<endl<<
+        "The results will be sent to the user in a tabular format and will then "
+        <<"be sent to text file."<<endl; 
 }
 
+/**
+ * 
+ * @param sat
+ */
 void read_scores(int sat[10][2])
 {
     for(int i = 0; i<10;i++)
@@ -49,13 +71,18 @@ void read_scores(int sat[10][2])
                 
             else{
                 cout<<"Enter Verbal Score below:"<<endl;
-            }
-                   
+            }    
             cin>>sat[i][j];  
         }
     }
 }
 
+/**
+ * 
+ * @param sat
+ * @param mathAvg
+ * @param verbAvg
+ */
 void compute_means(int sat[10][2], int& mathAvg, int& verbAvg)
 {
     int mathScoreSum =0;
@@ -77,14 +104,20 @@ void compute_means(int sat[10][2], int& mathAvg, int& verbAvg)
                 verbScoreSum += sat[i][j];
                 verbCount++;
             }
-            
         }
     }
-    
     mathAvg = mathScoreSum / mathCount;
     verbAvg = verbScoreSum / verbCount;
 }
 
+/**
+ * 
+ * @param sat
+ * @param mathAvg
+ * @param verbAvg
+ * @param mathStd
+ * @param verbStd
+ */
 void compute_std(int sat[10][2],int mathAvg, int verbAvg, double& mathStd, double& verbStd) 
 {
     double mathVariance = 0;
@@ -96,25 +129,37 @@ void compute_std(int sat[10][2],int mathAvg, int verbAvg, double& mathStd, doubl
         {
               
             if(j==0){
-                mathVariance += pow((sat[i][j] - mathAvg), 2);
+                //mathVariance += pow((sat[i][j] - mathAvg), 2);
             }
             
             else{
-                verbVariance += pow((sat[i][j] - mathAvg), 2);
+                //verbVariance += pow((sat[i][j] - mathAvg), 2);
             }
             
             mathStd = sqrt(mathVariance/9);
             verbStd = sqrt(verbVariance/9);
             
+            //testing to see if it works
+            cout<<mathStd<<endl<<verbStd;
         }
     }
 }
 
-/*void show_results(int sat[10][2], int mathAvg, int verbAvg, double mathStd,
- double verbstd)
+/**
+ * 
+ * @param sat
+ * @param mathAvg
+ * @param verbAvg
+ * @param mathStd
+ * @param verbstd
+ */
+void show_results(int sat[10][2], int mathAvg, int verbAvg, double mathStd, double verbstd)
 {
-    ofstream outs;
-    outs>>"Table of SAT Scores:\n";
+    ofstream file;
+    file.open("scores.txt");
+    file.close();
+    
+    /*outs>>"Table of SAT Scores:\n";
     outs>>"Math   Verbal\n";
     
     //Displays the variables inside sat array
@@ -125,23 +170,27 @@ void compute_std(int sat[10][2],int mathAvg, int verbAvg, double& mathStd, doubl
                 {
                     
                 }
-            }
-}*/
+            }*/
+}
 
-/*bool again()
+/**
+ * 
+ * @return Bool variable
+ */
+bool again()
 {
     char response;
-    do
-    {
-            cout<<"\nDo you wish to run this program again (Y or N)?\n";
-            cin>>response;
-            response = toupper(response);
-            if(response!='Y' && response!= 'N')
-                cout<<"Illegal response! Answer Y for Yes, N for No.\n";
-    }while(response!='Y' && response!='N');
-        if(response == 'Y')
-            return true;
+    
+    cout<<endl<<"Do you wish to run this program again (Y or N)?"<<endl;
+    cin>>response;
+    response = toupper(response);
+
+    while((response!='Y') || (response!= 'N'))
+        cout<<"Illegal response! Answer Y for Yes, N for No.\n";
+       
+    if(response == 'Y')
+        return true;
     else
         return false;    
-}*/
+}
 
